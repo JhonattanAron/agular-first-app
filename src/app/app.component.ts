b17fdf1d55fd9b01248144c4836b6979332137b1
Aron <aron.cachago@gmail.com>
2023-07-27 17:42:36 -0500

    conocer Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'agular-first-app';
+  title = 'Mi Primera App';
+  nombre = "Aron Cachago"
 }
