fb0700ace5c90fe9c83283eacc94b1f4b257f078
Aron <aron.cachago@gmail.com>
2023-08-07 20:08:52 -0500

    event binding

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -9,27 +9,40 @@ export class EmpleadoComponent {
   private nombre = "Juan"
   private apellido = "Diaz"
   private edad = 18
-  private empresa = "AdaptLeap"
+  private empresa:String = ""
   protected habilitacionCuadro = false;
   protected UsuRegistrado = true;
-
+  protected textoDeRegistro = "NO HAY NADIE REGISTRADO"
 
   protected getNombre(){
     return this.nombre
   }
   protected getApellido(){
-    return this.apellido
+    return this.apellido;
   }
   protected getEmpresa(){
-    return this.empresa
+    return this.empresa;
   }
 
   protected getEdad(){
-    return this.edad
+    return this.edad;
+  }
+
+  protected setUsuarioRegistro(event:Event){
+    //alert("El usuario se acaba de Registrar")
+    if ((<HTMLInputElement>event.target).value == "si") {
+      this.textoDeRegistro = "USUARIO REGISTRADO";
+    }else{
+      this.textoDeRegistro = "NO HAY NADIE REGISTRADO";
+    }
+  }
+  
+  protected guardarEmpresa(empresa: String){
+    this.empresa = empresa
   }
 
   protected getRegistroUsuario(){
-    this.UsuRegistrado = false
+    this.UsuRegistrado = false;
   }
   
 }
